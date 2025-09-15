fd5e7a85b0559462e13f1df9b587011037dcb382
KroegerLeif <Leif.kr@outlook.com>
2025-09-15 10:22:52 +0200

    Inital Commit

--- src/main/java/org/example/asterixapi_neuefische/AsterixApiNeueFischeApplication.java
+++ src/main/java/org/example/asterixapi_neuefische/AsterixApiNeueFischeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AsterixApiNeueFischeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AsterixApiNeueFischeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AsterixApiNeueFischeApplication.class, args);
+    }
 
 }
--- src/test/java/org/example/asterixapi_neuefische/AsterixApiNeueFischeApplicationTests.java
+++ src/test/java/org/example/asterixapi_neuefische/AsterixApiNeueFischeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AsterixApiNeueFischeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
